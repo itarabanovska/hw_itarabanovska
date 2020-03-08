8261b3268c11384d6d6a671f24636f6a86574ab5
itarabanovska <i.tarabanovska@gmail.com>
2020-03-08 20:01:48 +0200

    Homework 3

---
+++ hw3_2.py
@@ -0,0 +1,18 @@
+name = input("Please enter your name")
+print('Hello,' + name + '!')
+
+
+term1 = 2
+term2 = 5
+print(term1 + term2)
+print(term1 - term2)
+print(term1 * term2)
+print(term2 / term1)
+print(term1 ** term2)
+
+a = ['hjh', 'hjfhgj', 'kjkjspp', 'kqioewp', 'kjqhcvvvx', 'qazsxkb', 'lkjho', 'ujksdfjjjd', 'qapoiiz', 'oi']
+b = [100, 54, 3245, 987403, 74300, 1, 23456, 49032999934, 88345552, 5201234]
+
+
+
+
