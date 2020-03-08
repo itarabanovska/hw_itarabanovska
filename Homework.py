5619e57ec914bfde13a189791d9339b0cbc62d7e
itarabanovska <i.tarabanovska@gmail.com>
2020-03-08 13:24:26 +0200

    hw2_2, hw2_3, hw2_4

---
+++ Homework.py
@@ -0,0 +1,62 @@
+hw2_1
+
+i.tarabanovska@Eugenes-MBP Desktop % mkdir hw2_1
+i.tarabanovska@Eugenes-MBP Desktop % cd hw2_1
+i.tarabanovska@Eugenes-MBP hw2_1 % mkdir foo1 foo2 foo3 foo4
+i.tarabanovska@Eugenes-MBP hw2_1 % touch foo1/bar1.py foo2/bar2.py foo3/bar3.py foo4/bar4.py
+i.tarabanovska@Eugenes-MBP hw2_1 % cd foo4
+i.tarabanovska@Eugenes-MBP foo4 % mkdir spam
+i.tarabanovska@Eugenes-MBP foo4 % cd spam
+i.tarabanovska@Eugenes-MBP spam % touch eggs1.py eggs2.py
+
+
+hw2_2
+
+i.tarabanovska@Eugenes-MBP Desktop % mkdir hw2_2
+i.tarabanovska@Eugenes-MBP Desktop % cd hw2_2
+i.tarabanovska@Eugenes-MBP hw2_2 % git init
+i.tarabanovska@Eugenes-MBP hw2_2 % nano test.py
+i.tarabanovska@Eugenes-MBP hw2_2 % git add .
+i.tarabanovska@Eugenes-MBP hw2_2 % git commit -m "initial commit"
+i.tarabanovska@Eugenes-MBP hw2_2 % nano test.py
+i.tarabanovska@Eugenes-MBP hw2_2 % git add .
+i.tarabanovska@Eugenes-MBP hw2_2 % git commit -m "Some very interesting feature"
+i.tarabanovska@Eugenes-MBP hw2_2 % nano test.py
+i.tarabanovska@Eugenes-MBP hw2_2 % git add .
+i.tarabanovska@Eugenes-MBP hw2_2 % git commit -m "Another cool feature"
+i.tarabanovska@Eugenes-MBP hw2_2 % nano test.py
+i.tarabanovska@Eugenes-MBP hw2_2 % git add .
+i.tarabanovska@Eugenes-MBP hw2_2 % git commit -m "We are feature-complete \o/"
+
+hw2_3
+
+i.tarabanovska@Eugenes-MBP Desktop % mkdir hw2_3
+i.tarabanovska@Eugenes-MBP Desktop % cd hw2_3
+i.tarabanovska@Eugenes-MBP hw2_3 % git init
+i.tarabanovska@Eugenes-MBP hw2_3 % nano chapter.py
+i.tarabanovska@Eugenes-MBP hw2_3 % git add .
+i.tarabanovska@Eugenes-MBP hw2_3 % git commit -m "initial commit"
+i.tarabanovska@Eugenes-MBP hw2_3 % nano chapter.py
+i.tarabanovska@Eugenes-MBP hw2_3 % git add .
+i.tarabanovska@Eugenes-MBP hw2_3 % git commit -m "working piece of code"
+i.tarabanovska@Eugenes-MBP hw2_3 % nano chapter.py
+i.tarabanovska@Eugenes-MBP hw2_3 % git add .
+i.tarabanovska@Eugenes-MBP hw2_3 % git commit -m "Ready for release :glasses:"
+i.tarabanovska@Eugenes-MBP hw2_3 % git checkout HEAD^
+
+hw2_4
+i.tarabanovska@Eugenes-MBP Desktop % mkdir hw2_4
+i.tarabanovska@Eugenes-MBP Desktop % cd hw2_4
+i.tarabanovska@Eugenes-MBP hw2_4 % git init
+i.tarabanovska@Eugenes-MBP hw2_4 % nano finish.py
+i.tarabanovska@Eugenes-MBP hw2_4 % git add .
+i.tarabanovska@Eugenes-MBP hw2_4 % git commit -m "initial commit"
+i.tarabanovska@Eugenes-MBP hw2_4 % nano finish.py
+i.tarabanovska@Eugenes-MBP hw2_4 % git add .
+i.tarabanovska@Eugenes-MBP hw2_4 % git commit -m "Thats a feature everybody will definitly enjoy"
+i.tarabanovska@Eugenes-MBP hw2_4 % nano finish.py
+i.tarabanovska@Eugenes-MBP hw2_4 % git add .
+i.tarabanovska@Eugenes-MBP hw2_4 % git commit -m "Feeling risky" 
+i.tarabanovska@Eugenes-MBP hw2_4 % git checkout master^
+
+done)
